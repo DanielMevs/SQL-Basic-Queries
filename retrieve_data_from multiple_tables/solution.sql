d60a08e48228c90c34f53fd015c84ac727bab02f
DanielMevs <dannymevs@hotmail.com>
2023-09-22 00:07:26 -0400

    minor refactor

--- retrieve_data_from multiple_tables/solution.sql
+++ retrieve_data_from multiple_tables/solution.sql
@@ -1,7 +1,6 @@
 -- Attempt 1
-SELECT r.DATE, r.PartySize, c.FirstName, c.LastName, c.Email 
-FROM Customers AS c
-INNER JOIN Reservations AS r
-ON c.CUSTOMERID = r.CUSTOMERID
-ORDER BY r.DATE DESC
-LIMIT 2;
+SELECT TOP 2 Reservations.Date, Reservations.PartySize,
+    Customers.FirstName, Customers.LastName, Customers.Email
+FROM Reservations INNER JOIN Customers
+ON Reservations.CustomerID = Customers.CustomerID
+ORDER BY Date DESC;
